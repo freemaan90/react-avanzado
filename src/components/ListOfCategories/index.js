b771b2678004c917058481acb334093d296988fa
Frederico Zanoni <fredericozanoni@gmail.com>
2020-09-17 10:16:56 -0300

    Preparando hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Category } from '../category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState(mockCategories)
   return (
     <List>
       {
