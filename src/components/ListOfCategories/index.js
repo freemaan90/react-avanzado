8ade9cbd81db9d1b37017341d6c7260f1330198b
Frederico Zanoni <fredericozanoni@gmail.com>
2020-09-17 11:21:09 -0300

    Resolviendo hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../category'
 import { List, Item } from './styles'
-import { categories as mockCategories } from '../../../api/db.json'
+
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(function () {
+    setLoading(true)
+    window.fetch('https://react-avanzado.freemaan90.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+  return { categories, loading }
+}
 
 export const ListOfCategories = () => {
-  const [categories, setCategories] = useState(mockCategories)
-  return (
-    <List>
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
         categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+  if (loading) {
+    return 'Cargando..'
+  }
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
